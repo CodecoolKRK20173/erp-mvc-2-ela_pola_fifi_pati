21ac6b7a9b35f9038e602bdb88862f92cbdf0c3b
Patryczkov <patryczkov@gmail.com>
2019-03-12 09:39:42 +0100

    Added working menu in CRM_controller

--- controller/crm_controller.py
+++ controller/crm_controller.py
@@ -13,4 +13,26 @@ def run():
         None
     """
 
-    # your code
+    title = "Customer Relationship Management (CRM)"
+    options = ["Add item",
+                "Remove item",
+                "Update item",
+                "Longest ID of customer",
+                "Show subscibers"]
+    exit_message = "Back to main menu"
+    
+    choice = None
+    while choice != "0":
+        choice = terminal_view.get_choice(title, options, exit_message)
+        if choice == "1":
+            crm.add()
+        elif choice == "2":
+            crm.remove()
+        elif choice == "3":
+            crm.update()
+        elif choice == "4":
+            crm.get_longest_name_id()
+        elif choice == "5":
+            crm.get_subscribed_emails()
+        else:
+            terminal_view.print_error_message("There is no such choice.")
