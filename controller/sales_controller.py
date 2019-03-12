7294048a8b9ff56e0ab7214b13c801d27714c545
Patryczkov <patryczkov@gmail.com>
2019-03-12 10:01:28 +0100

    Added working menu for sales_controller

--- controller/sales_controller.py
+++ controller/sales_controller.py
@@ -13,4 +13,26 @@ def run():
         None
     """
 
-    # your code
+    title = "Sales manager"
+    options = ["Add item",
+                "Remove item",
+                "Update item",
+                "Show ID of item sold for lowest price",
+                "Show item sold between certain dates"]
+    exit_message = "Back to main menu"
+    
+    choice = None
+    while choice != "0":
+        choice = terminal_view.get_choice(title, options, exit_message)
+        if choice == "1":
+            sales.add()
+        elif choice == "2":
+            sales.remove()
+        elif choice == "3":
+            sales.update()
+        elif choice == "4":
+            sales.get_lowest_price_item_id()
+        elif choice == "5":
+            sales.get_items_sold_between()
+        else:
+            terminal_view.print_error_message("There is no such choice.")
