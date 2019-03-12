256ea16b715929daf435ffaea6e1e963acddaa34
aMomentLikeThis <emyczkow@poczta.fm>
2019-03-12 09:57:07 +0100

    added inventory controller logic

--- controller/inventory_controller.py
+++ controller/inventory_controller.py
@@ -13,4 +13,26 @@ def run():
         None
     """
 
-    #xd
+    title = "Inventory manager"
+    options = ["Add item",
+                "Remove item",
+                "Update item",
+                "Show items that have not exceeded their durability",
+                "Show average durability times for each manufacturer"]
+    exit_message = "Back to main menu"
+    
+    choice = None
+    while choice != "0":
+        choice = terminal_view.get_choice(title, options, exit_message)
+        if choice == "1":
+            inventory.add()
+        elif choice == "2":
+            inventory.remove()
+        elif choice == "3":
+            inventory.update()
+        elif choice == "4":
+            inventory.get_available_items()
+        elif choice == "5":
+            inventory.get_average_durability_by_manufacturers()
+        else:
+            terminal_view.print_error_message("There is no such choice.")
