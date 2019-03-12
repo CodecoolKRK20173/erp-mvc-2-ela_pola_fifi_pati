2417c4d86a1424adfe4d1279318d110126f00dd2
Patryczkov <patryczkov@gmail.com>
2019-03-12 09:47:18 +0100

    Added working menu in hr_controller

--- controller/hr_controller.py
+++ controller/hr_controller.py
@@ -13,4 +13,26 @@ def run():
         None
     """
 
-    # your code
+    title = "Human resources manager"
+    options = ["Add item",
+                "Remove item",
+                "Update item",
+                "Show the oldest person",
+                "Show person closest to average age"]
+    exit_message = "Back to main menu"
+    
+    choice = None
+    while choice != "0":
+        choice = terminal_view.get_choice(title, options, exit_message)
+        if choice == "1":
+            hr.add()
+        elif choice == "2":
+            hr.remove()
+        elif choice == "3":
+            hr.update()
+        elif choice == "4":
+            hr.get_oldest_person()
+        elif choice == "5":
+            hr.get_persons_closest_to_average()
+        else:
+            terminal_view.print_error_message("There is no such choice.")
