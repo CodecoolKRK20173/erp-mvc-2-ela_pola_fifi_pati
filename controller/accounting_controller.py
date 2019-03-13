ddf8ab330c1f7fd92ece8b0c3607daf509491adb
Patryczkov <patryczkov@gmail.com>
2019-03-13 18:27:41 +0100

    Added working printing of results for bonus funcs

--- controller/accounting_controller.py
+++ controller/accounting_controller.py
@@ -4,6 +4,7 @@ from model.accounting import accounting
 from controller import common
 from model import data_manager
 
+
 def run():
     """
     Starts this module and displays its menu.
@@ -15,10 +16,10 @@ def run():
     """
     title = "Accounting options"
     options = ["Add item",
-                "Remove item",
-                "Update item",
-                "Which year has a maximum profit",
-                "What is an avarange profit in a given year"]
+               "Remove item",
+               "Update item",
+               "Which year has a maximum profit",
+               "What is an avarange profit in a given year"]
     exit_message = "Back to main menu"
 
     title_list = [
@@ -29,27 +30,27 @@ def run():
         ['Type'],
         ['Amount']]
 
-    
     data_file = "model/accounting/items.csv"
     table = data_manager.get_table_from_file(data_file)
     terminal_view.print_table(table, title_list)
-
-    
+    #
     choice = None
     while choice != "0":
         choice = terminal_view.get_choice(title, options, exit_message)
         if choice == "1":
-            accounting.add()
+            #rozpierdala tabelkę, zrobić backup pliku .csv
+            record = terminal_view.get_inputs(["Month"] + ["Day"] + ["Year"] + ["Type"] + ["Amount"], "")
+            accounting.add(table, record)
         elif choice == "2":
             accounting.remove()
         elif choice == "3":
             accounting.update()
         elif choice == "4":
-            accounting.which_year_max()
+            terminal_view.print_result(accounting.which_year_max(table), "Year with the highest income: ")
         elif choice == "5":
-            accounting.avg_amount()
+            year = terminal_view.get_inputs(['Year to get average: '], "")
+            terminal_view.print_result(accounting.avg_amount(table, year[0]), "Average: ")
         else:
             terminal_view.print_error_message("There is no such choice.")
-                
-   
+
     # your code
