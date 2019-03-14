41fb9877f5f8e1cda926e7ee9193042ec5580002
Patryczkov <patryczkov@gmail.com>
2019-03-14 12:57:40 +0100

    Added working bonus funcs

--- model/sales/sales.py
+++ model/sales/sales.py
@@ -55,6 +55,7 @@ def remove(table, id_):
     data_manager.write_table_to_file("model/accounting/items.csv", table)
     return table
 
+
 def update(table, id_, record):
     """
     Updates specified record in the table.
@@ -73,7 +74,6 @@ def update(table, id_, record):
     table.append(record)
     data_manager.write_table_to_file("model/sales/sales.csv", table)
 
-
     entry_index = 0
     for entry in table:
         entry_id_ = entry[0]
@@ -85,6 +85,7 @@ def update(table, id_, record):
 
 # special functions:
 # ------------------
+
 
 def get_lowest_price_item_id(table):
     """
@@ -98,7 +99,24 @@ def get_lowest_price_item_id(table):
          string: id
     """
 
-    # your code
+    index_index = 0
+    index_title = 1
+    index_price = 2
+    index_first_game = 0
+    min_price = int(table[index_first_game][index_price])
+    lowest_price_games = []
+    for game in table:
+        if int(game[index_price]) < min_price:
+            min_price = int(game[index_price])
+    for game in table:
+        if int(game[index_price]) == min_price:
+            lowest_price_games.append(game)
+    if len(lowest_price_games) > 1:
+        list_titles = sort_my([game[index_title] for game in lowest_price_games])
+        for game in lowest_price_games:
+            if game[index_title] == list_titles[-1]:
+                return game[index_index]
+    return lowest_price_games[index_index][index_index]
 
 
 def get_items_sold_between(table, month_from, day_from, year_from, month_to, day_to, year_to):
@@ -117,5 +135,79 @@ def get_items_sold_between(table, month_from, day_from, year_from, month_to, day
     Returns:
         list: list of lists (the filtered table)
     """
+    filtered_table = []
+    month_from = str(month_from)
+    month_to = str(month_to)
+    day_from = str(day_from)
+    day_to = str(day_to)
+    year_from = str(year_from)
+    year_to = str(year_to)
+    if len(month_to) == 1:
+        month_to = "0" + month_to
+    if len(month_from) == 1:
+        month_from = "0" + month_from
+    if len(day_to) == 1:
+        day_to = "0" + day_to
+    if len(day_from) == 1:
+        day_from = "0" + day_from
+    from_number = int(year_from + month_from + day_from)
+    to_number = int(year_to + month_to + day_to)
 
-    # your code
+    for element in table[:]:
+        if len(element[3]) == 1:
+            element[3] = "0" + element[3]
+        if len(element[4]) == 1:
+            element[4] = "0" + element[4]
+        item_number = int(element[5] + element[3] + element[4])
+        if item_number > from_number and item_number < to_number:
+            filtered_table.append(element)
+
+    for i in range(len(filtered_table)):
+        filtered_table[i][2] = int(filtered_table[i][2])
+        filtered_table[i][3] = int(filtered_table[i][3])
+        filtered_table[i][4] = int(filtered_table[i][4])
+        filtered_table[i][5] = int(filtered_table[i][5])
+    return filtered_table
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+    index_month = 3
+    index_day = 4
+    index_year = 5
+    games_from_to = []
+    for game in table:
+        if year_from < int(game[index_year]) < year_to:
+            games_from_to.append(game)
+        elif year_from <= int(game[index_year]) <= year_to:
+            if month_from < int(game[index_month]) < month_to:
+                games_from_to.append(game)
+            elif int(game[index_month]) == month_from:
+                if int(game[index_day]) > day_from:
+                    games_from_to.append(game)
+            elif int(game[index_month]) == month_to:
+                if int(game[index_day]) < day_to:
+                    games_from_to.append(game)
+    games_from_to = [item[:-1] for item in games_from_to]
+
+    converted_list = []
+    for game in games_from_to:
+        item_list = []
+        for item in game:
+            if item.isdigit():
+                item_list.append(int(item))
+            else:
+                item_list.append(item)
+        converted_list.append(item_list)
+
+    return converted_list
+"""
